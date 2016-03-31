c62c30783674e914bdaf12b31974afe4e523708a
Travis Hilterbrand <thilterbrand@five9.com>
2016-03-31 01:44:54 -0700

    extract warm-transfer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Framework from './Framework';
 
+import CallControlsWarmTransfer from './CallControlsWarmTransfer';
 
 var Model = Backbone.Model.extend({
   completeTransfer: function(e) {
@@ -12,77 +13,11 @@ var Model = Backbone.Model.extend({
     alert('Cancel Transfer [' + this.get('email') + ']');
   }
 });
-
 var myModel = new Model({
   email: 'test@test.com',
   isSoftphone: true,
   isActionInProgress: false
 });
-
-var KeypadPanel = Framework.createReactClass({
-  componentName: 'KeypadPanel',
-  getInitialState: function() {
-    return {
-      showKeypad: true
-    };
-  },
-  onShowKeypadClicked: function(e) {
-    this.setState({'showKeypad': !this.state.showKeypad});
-  },
-  onRender: function(data, modelOrCollection, helpers) {
-    return (
-<div>
-  <div className="row">
-    <button onClick={this.onShowKeypadClicked}>Show Keypad</button>
-  </div>
-  <If condition={this.state.showKeypad}>
-    <div className="row">KEYPAD</div>
-  </If>
-  <hr />
-</div>
-    );
-  }
-});
-
-var CallControlsWarmTransfer = Framework.createReactClass({
-  componentName: 'CallControlsWarmTransfer',
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    this.refs.myInput.focus();
-  },
-  componentWillUnmount: function() {},
-  onRender: function(data, modelOrCollection, helpers) {
-  return (
-<div>
-  <input ref="myInput" />
-  <div className="row group-separator-container">
-    <div className="group-separator-title">
-      <span>{data.email}</span>
-    </div>
-  </div>
-  <hr />
-  <div className="row group-separator-container">
-    <div className="group-separator-title">
-      <span>Warm Transfer</span>
-    </div>
-  </div>
-  <If condition={data.isSoftphone}>
-    <KeypadPanel model={modelOrCollection} />
-  </If>
-  <div className="row">
-{/* commented out */}
-    <button onClick={modelOrCollection.completeTransfer.bind(modelOrCollection)}>Complete Transfer</button>
-  </div>
-  <div className="row">
-    <button onClick={modelOrCollection.cancelTransfer.bind(modelOrCollection)}>Cancel Transfer</button>
-  </div>
-</div>
-    );
-  }
-});
-
 
 export default class App extends Component {
   render() {
